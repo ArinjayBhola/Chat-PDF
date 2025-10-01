a139cd2169fcae82b7fe7773b06c1eb53ba47630
ArinjayBhola <arinjay26bhola@gmail.com>
2025-10-01 13:27:05 +0000

    aws s3 added

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { uploadToS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
@@ -10,8 +11,19 @@ const FileUpload = () => {
       "application/pdf": [".pdf"],
     },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("File size should be less than 10MB");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
   return (
