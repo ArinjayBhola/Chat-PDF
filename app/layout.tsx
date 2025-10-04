b8b512d09ac2df286993adc29633b76cbeacc967
ArinjayBhola <arinjay26bhola@gmail.com>
2025-10-04 20:42:27 +0530

    react-query add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Provider from "@/components/Provider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-      </html>
+      <Provider>
+        <html lang="en">
+          <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+        </html>
+        <Toaster />
+      </Provider>
     </ClerkProvider>
   );
 }
